fabf7b5f7c56431560ade93b496b83f621376c7d
PallaviKatari <pallavikatari@gmail.com>
2022-03-26 10:30:19 +0530

    Commit_Interface_Exceptions

---
+++ Exceptions.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NIIT
+{
+    //Errors->Syntax, Compile time, Runtime, Logical(bonus-company)
+    //Runtime error-To handle the runtime errors - Exception handling 
+    //Exception Handling->try,catch,finally
+
+    //C#- Exception Class -Base class - all the pre-defined exception classes
+    //Pre-defined- DivideByZeroException,NullReference etc
+
+    //Custom-Exception(User-defined) -Developer can create an owm exception class
+    internal class Exceptions
+    {
+        int result=0;
+        /// <summary>
+        /// DivideByZeroException
+        /// </summary>
+        public void Divide(int a,int b)
+        {
+            //try block-holds the set of doubtful codes
+            try
+            {
+                result=a/b;
+            }
+            //Handle the exception
+            catch(DivideByZeroException)
+            {
+                Console.WriteLine("You cannot divide by zero");
+            }
+            //Displays either exception is raised or not
+            finally
+            {
+                Console.WriteLine("Result is :"+result);
+            }
+
+        }
+
+        public void ArrayIndex()
+        {
+            try
+            {
+                int[] numbers = {1,2,3,4,5,6,7};
+                Console.WriteLine(numbers[1]);
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        public static void Main()
+        {
+            Exceptions ex = new Exceptions();   
+            ex.Divide(10,0);
+            ex.ArrayIndex();
+        }
+    }
+}
---
+++ Interface.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NIIT
+{
+    //Interface- keyword-group of abstract methods and properties-
+    //implemented in the derived classes
+
+    //Interface-no variables/fields
+    //Interface-Complete Abstraction- Security
+    //C#-Multiple Inheritance is not supported - overcome by multiple interface
+    interface ICourse
+    {
+        //abstract property
+        string Name { get; set; }
+        //abstract method
+        //Method Declaration
+        void Details();
+    }
+    interface ICourseList
+    {
+        void CourseDescription();
+    }
+    //implement an interface
+    class Institution:ICourse,ICourseList
+    {
+        public string Name { get; set; }
+
+        //Method Definition
+        public void Details()
+        {
+            Console.WriteLine("Name is:" + Name);
+            Console.WriteLine("CSharp Programming");
+        }
+        public void CourseDescription()
+        {
+            Console.WriteLine("CSharp Programming in .Core Environment");
+        }
+    }
+    internal class Interface
+    {
+        public static void Main()
+        {
+            Institution institution = new Institution();
+            institution.Name = "John";
+            institution.Details();
+            institution.CourseDescription();
+        }
+    }
+}
