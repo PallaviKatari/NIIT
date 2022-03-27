a2716f6d68643ed9fbbe0c4ed9380d1d01159ee6
PallaviKatari <pallavikatari@gmail.com>
2022-03-27 15:15:01 +0530

    Commit_SerializeDeserialize_Attributes

---
+++ Attributes.cs
@@ -0,0 +1,46 @@
+﻿//Directive
+#define DEBUG
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace NIIT
+{
+    /// <summary>
+    /// Attributes-declarative information of metadata-class,method,property,types,assembly etc
+    /// Pre-defined-AttributeUsage,Conditional,Obsolete
+    /// User-defined - create our own attributes which inherits from the base class- Attribute
+    /// </summary>
+    internal class Attributes
+    {
+        [Obsolete("Use NewMethod, I am of no use anymore",true)]
+        static void OldMethod()
+        {
+            Console.WriteLine("I am not in use anymore!!!");
+        }
+        static void NewMethod()
+        {
+            Console.WriteLine("I am the one who serves your requirement!!!");
+        }
+
+        public static void Main()
+        {
+            //OldMethod();
+            NewMethod();
+            Attributes1.Details("John");
+        }
+    }
+
+    internal class Attributes1
+    {
+        [Conditional("DEBUG")]
+
+        public static void Details(string name)
+        {
+            Console.WriteLine(name);
+        }
+    }
+}
---
+++ Files4.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+//Serialization
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoApplication
+{
+    //Serializable is class->Attribute
+    //sealed-to restrict other classes from inheriting a class
+    [Serializable]
+    class Files
+    {
+        //Variables
+        public int ID;
+        public String Name;
+        public String Designation;
+        static void Main(string[] args)
+        {
+            Files obj = new Files();
+            obj.ID = 1;
+            obj.Name = "John";
+            obj.Designation = "HR";
+
+            //Encryption and Decryption
+            //IFormatter->Interface(Serialize and Deserialize),BinaryFormatter(class)->inheriting->IFormatter
+           
+            //Base->Derived
+            IFormatter formatter = new BinaryFormatter();
+
+            //Stream->abstract class,Filestream(class)-inheriting the stream class
+            //Base->Derived
+            Stream stream = new FileStream(@"E:\ExampleNew.txt", FileMode.Create, FileAccess.Write);
+
+            //stream->ExampleNew.txt  obj->ID and Name
+            formatter.Serialize(stream, obj);
+            stream.Close();
+
+            //Base->Derived
+            stream = new FileStream(@"E:\ExampleNew.txt", FileMode.Open, FileAccess.Read);
+
+            //Files-user-defined class
+            //(Files)->typecasting the content into class object
+            Files objnew = (Files)formatter.Deserialize(stream);
+
+            Console.WriteLine(objnew.ID);
+            Console.WriteLine(objnew.Name);
+            Console.WriteLine(objnew.Designation);
+
+            Console.ReadKey();
+        }
+    }
+}
