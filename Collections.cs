0ce8451d4327f09d686e3b37b43786b1a4389fb9
PallaviKatari <pallavikatari@gmail.com>
2022-04-02 15:28:45 +0530

    Commit_Generics_Collections

---
+++ Collections.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NIIT
+{
+    //Collections-type of class-dynamic memory allocation-store/retrieve-other operations
+    //System.Collections
+    //System.Collections.Generic
+    //Collections-replicate the data structure of an array
+    internal class Collections
+    {
+        public static void Main()
+        {
+            ArrayListDemo();
+            ListDemo();
+            SortedListDemo();
+            LinkedListDemo();
+            StackDemo();
+            QueueDemo();
+            DictionaryDemo(); 
+            HashtableDemo();
+            BitArrayDemo();
+
+        }
+        //1. ARRAYLIST-SYSTEM.COLLECTIONS
+        public static void ArrayListDemo()
+        {
+            ArrayList arrayList = new ArrayList();
+            arrayList.Add(1);
+            arrayList.Add(2);
+            arrayList.Add(3);
+            arrayList.Add(4.5);
+            arrayList.Add("Hello");
+            Console.WriteLine("ARRAYLIST");
+            foreach (var i in arrayList)
+            {
+                Console.WriteLine(i);
+            }
+            arrayList.Remove(1);
+            foreach (var i in arrayList)
+            {
+                Console.WriteLine(i);
+            }
+            arrayList.Clear();
+            foreach (var i in arrayList)
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+
+        //2. LIST<> - SYSTEM.COLLECTIONS.GENERIC
+        public static void ListDemo()
+        {
+            List<int> list = new List<int>();
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Insert(0, 4);
+            Console.WriteLine("LIST");
+            foreach(var i in list)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        //3. SORTEDLIST<> - key/value pair
+
+        public static void SortedListDemo()
+        {
+            SortedList<string,int> list = new SortedList<string,int>();
+            list.Add("CSHARP",1);
+            list.Add("HTML",2);
+            list.Add("CSS",3);
+            Console.WriteLine("SORTEDLIST");
+            foreach (var s in list)
+            {
+                Console.WriteLine(s);
+            }
+
+        }
+
+        //4. LINKEDLIST<>
+
+        public static void LinkedListDemo()
+        {
+            LinkedList<int> list = new LinkedList<int>();
+            list.AddLast(1);
+            list.AddLast(2);
+            list.AddFirst(0);
+            Console.WriteLine("LINKEDLIST");
+            foreach (int i in list)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        //5. STACK(LIFO)
+
+        public static void StackDemo()
+        {
+            Stack<int> list = new Stack<int>();
+            list.Push(1);
+            list.Push(2);
+            list.Push(3);
+            list.Pop();
+            Console.WriteLine("STACK");
+            Console.WriteLine("Top Element is:{0}", list.Peek());
+            foreach (int i in list)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        //6. QUEUE(FIFO)
+
+        public static void QueueDemo()
+        {
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+            queue.Dequeue();
+            Console.WriteLine("QUEUE");
+            Console.WriteLine("Top Element is:{0}", queue.Peek());
+            foreach (int i in queue)
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+
+        //7. DICTIONARY - KEY/VALUE PAIR
+        public static void DictionaryDemo()
+        {
+            Dictionary<int,string> course = new Dictionary<int,string>();
+            course.Add(1, "C#");
+            course.Add(2, "SQL");
+            course.Add(3, "ASP.NET");
+            //course.Add(4, 5);
+            Console.WriteLine("DICTIONARY");
+            foreach (var i in course)
+            {
+                Console.WriteLine(i);
+            }
+            var key=new List<int>(course.Keys);
+            foreach(var i in key)
+            {
+                Console.WriteLine(i);
+            }
+            var values=new List<string>(course.Values);
+            foreach (var j in values)
+            {
+                Console.WriteLine(j);
+            }
+            //Console.WriteLine(key[4]);
+
+        }
+
+        // 8. HASHTABLE-KEY/VALUE PAIR- NON-GENERIC
+
+        public static void HashtableDemo()
+        {
+            Hashtable ht=new Hashtable();
+            ht.Add(1, "C#");
+            ht.Add(2, "SQL");
+            ht.Add(3, "ASP.NET");
+            ht.Add(4, 5);
+            Console.WriteLine("HASHTABLE");
+            //Interface
+            ICollection key=ht.Keys;
+            foreach(var i in key)
+            {
+                Console.WriteLine(ht[i]);
+            }
+
+            Console.WriteLine("Key is:{0}", ht[5]);
+
+        }
+
+        //9. BITARRAY - NON GENERIC CLASS 
+        //0-FALSE  1-TRUE
+        public static void BitArrayDemo()
+        {
+            BitArray b1 = new BitArray(4);
+            BitArray b2 = new BitArray(4);
+            
+            byte[] a = { 0 }; //0000
+            byte[] b = { 15 }; //1111
+
+            b1= new BitArray(a); //0
+            b2= new BitArray(b); //15
+            Console.WriteLine("b1");
+            //0-0000-FFFF
+            for (int i=0;i<4;i++)
+            {
+                Console.WriteLine(b1[i]);
+            }
+            Console.WriteLine("b2");
+            //15-1111-TTTT
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine(b2[i]);
+            }
+            //AND  0000and1111=>0000=ffff
+            BitArray b3 = new BitArray(4);
+            b3= b1.And(b2);
+            Console.WriteLine("b1 AND b2");
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine(b3[i]);
+            }
+            //OR 0000OR1111=>1111=TTTT
+            b3 = b1.Or(b2);
+            Console.WriteLine("b1 OR b2");
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine(b3[i]);
+            }
+
+        }
+    }
+}
---
+++ Generics.cs
@@ -0,0 +1,254 @@
+﻿//Generics allow you to define the specification of the data type of
+
+//programming elements in a class or a method,
+
+//until it is actually used in the program. 
+
+//In other words, generics allow you to write a class or method that can work with any data type.
+
+//You write the specifications for the class or the method, with substitute parameters for data types. 
+
+//When the compiler encounters a constructor for the class or a function call for the method,
+
+//it generates code to handle the specific data type. 
+
+//using System;
+//namespace CSharpProgram
+//{
+//    //<GenericType>
+//    //classname->GenericClass<DataType>
+//    class GenericClass<T> // 1st call-string
+//    {
+//        public GenericClass(T msg) //1st call-string
+//        {
+//            Console.WriteLine(msg); //1st call-This ia a generic class
+//        }
+//        public void Show(T msg) //1st call-This is a generic method
+//        {
+//            Console.WriteLine(msg);
+//        }
+//    }
+//    class ProgramGeneric
+//    {
+//        static void Main(string[] args)
+//        {
+//            //Passing string datatype
+//            GenericClass<string> gen = new GenericClass<string>("This is generic class");
+//            GenericClass<int> genI = new GenericClass<int>(101);
+//            GenericClass<char> getCh = new GenericClass<char>('I');
+//            gen.Show("This is generic method");
+//            genI.Show(101);
+//            getCh.Show('I');
+//        }
+//    }
+//}
+//----------------------------------------------------------------------------
+//using System;
+//namespace GenericsDemo
+//{
+//    public class ClsMain
+//    {
+//        private static void Main()
+//        {
+//            bool IsEqual = ClsCalculator.AreEqual(10, "xyz");//false
+//            bool IsEqual1 = ClsCalculator.AreEqual("ABC", "ABC");//TRUE
+//            if (IsEqual)
+//            {
+//                Console.WriteLine("Both are Equal");
+//            }
+//            else
+//            {
+//                Console.WriteLine("Both are Not Equal");
+//            }
+//            if (IsEqual1)
+//            {
+//                Console.WriteLine("Both are Equal");
+//            }
+//            else
+//            {
+//                Console.WriteLine("Both are Not Equal");
+//            }
+//            Console.ReadKey();
+//        }
+//    }
+//    public class ClsCalculator
+//    {
+//        //Now this method can accept any data type
+//        public static bool AreEqual(object value1, object value2)//object reference 10 and 20
+//        {
+//            return value1 == value2; //10==20 ABC==ABC
+//        }
+//    }}
+
+//object-reference type
+//int-value type
+//value to reference - BOXING
+//ref to value- Unboxing
+
+//Let’s see the problem of the above code implementation.
+//We get poor Performance due to boxing and unboxing. The object type needs to be converted to the value type.
+//Now, the AreEuqal() method is not type - safe.Now it is possible to pass a string value for the first parameter and an integer value for the second parameter. 
+
+//namespace GenericsDemo
+//{
+//    public class ClsMain
+//    {
+//        private static void Main()
+//        {
+//            bool IsEqual1 = ClsCalculator.AreEqual(10, 20);
+//            bool IsEqual2 = ClsCalculator.AreEqual("ABC", "ABC");
+//            bool IsEqual3 = ClsCalculator.AreEqual(10.5, 20.5);
+//            if (IsEqual1)
+//            {
+//                Console.WriteLine("Both are Equal");
+//            }
+//            else
+//            {
+//                Console.WriteLine("Both are Not Equal");
+//            }
+//            if (IsEqual2)
+//            {
+//                Console.WriteLine("Both are Equal");
+//            }
+//            else
+//            {
+//                Console.WriteLine("Both are Not Equal");
+//            }
+//            if (IsEqual3)
+//            {
+//                Console.WriteLine("Both are Equal");
+//            }
+//            else
+//            {
+//                Console.WriteLine("Both are Not Equal");
+//            }
+//            Console.ReadKey();
+//        }
+//    }
+//    public class ClsCalculator //Method Overloading
+//    {
+//        public static bool AreEqual(int value1, int value2)
+//        {
+//            return value1 == value2;
+//        }
+//        public static bool AreEqual(string value1, string value2)
+//        {
+//            return value1 == value2;
+//        }
+//        public static bool AreEqual(double value1, double value2)
+//        {
+//            return value1 == value2;
+//        }
+//    }
+//}
+//The problem with the above code implementation is that we are repeating the same logic in each and every method.
+//However, if tomorrow we need to compare two float or two long values then again we need to create two more methods.
+
+
+//We can solve all the above problems with Generics in C#.
+//With generics, we will make the AreEqual() method to works with different types of data types.
+//namespace GenericsDemo
+//{
+//    public class ClsMain
+//    {
+//        private static void Main()
+//        {
+//            bool IsEqual = ClsCalculator.AreEqual<int>(10, 20);
+//            bool IsEqual = ClsCalculator.AreEqual<string>("ABC", "ABC");
+//            bool IsEqual = ClsCalculator.AreEqual<double>(10, 20.5);
+//            if (IsEqual)
+//            {
+//                Console.WriteLine("Both are Equal");
+//            }
+//            else
+//            {
+//                Console.WriteLine("Both are Not Equal");
+//            }
+//            Console.ReadKey();
+//        }
+//    }
+//    public class ClsCalculator
+//    {
+//        public static bool AreEqual<T>(T value1, T value2)
+//        {
+//            return value1.Equals(value2);
+//        }
+//    }
+//}
+//------------------------------------------------------------
+//using System;
+//namespace GenericsDemo
+//{
+//    public class ClsMain
+//    {
+//        private static void Main()
+//        {
+//            bool IsEqual = ClsCalculator.AreEqual<string, int>("Hello", 10);
+//            //bool IsEqual = ClsCalculator.AreEqual<string>("ABC", "ABC");
+//            //bool IsEqual = ClsCalculator.AreEqual<double>(10, 20.5);
+//            if (IsEqual)
+//            {
+//                Console.WriteLine("Both are Equal");
+//            }
+//            else
+//            {
+//                Console.WriteLine("Both are Not Equal");
+//            }
+//            Console.ReadKey();
+//        }
+//    }
+//    public class ClsCalculator
+//    {
+//        public static bool AreEqual<T, T1>(T value1, T1 value2)
+//        {
+//            return value1.Equals(value2);
+//        }
+//    }
+//}
+//-----------------------------------------------------------------
+using System;
+namespace GenericsDemo
+{
+    //Let us create a generic class with a generic constructor, generic member variable, generic property,
+    //and a generic method as shown below.
+
+    //MyGenericClass is a Generic Class
+    class MyGenericClass<T> //Generic type
+    {
+        //Generic variable
+        //The data type is generic
+        private T genericMemberVariable; //int
+        
+        //Generic Constructor
+        //Constructor accepts one parameter of Generic type
+        public MyGenericClass(T value) //int 10
+        {
+            genericMemberVariable = value;  //10
+        }
+        //Generic Method
+        //Method accepts one Generic type Parameter
+        //Method return type also Generic
+        public T genericMethod(T genericParameter) //int 200
+        {
+            Console.WriteLine("Parameter type: {0}, value: {1}", typeof(T).ToString(), genericParameter);//int 200
+            Console.WriteLine("Return type: {0}, value: {1}", typeof(T).ToString(), genericMemberVariable); //int 10
+            return genericMemberVariable; //10
+        }
+        //Generic Property
+        //The data type is generic
+        public T genericProperty { get; set; }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            MyGenericClass<int> integerGenericClass = new MyGenericClass<int>(10);
+            int val = integerGenericClass.genericMethod(200);//10
+            Console.WriteLine(val); //10
+            MyGenericClass<string> stringGenericClass = new MyGenericClass<string>("hello");
+            string val1 = stringGenericClass.genericMethod("World");
+            Console.WriteLine(val1);
+            Console.ReadKey();
+        }
+    }
+}
