0761876d64e8f2e36bd9d9b09450b81dbe8fd04e
PallaviKatari <pallavikatari@gmail.com>
2022-04-03 10:45:47 +0530

    Commit_NUnitTest

---
+++ UnitTestDemo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NIIT
+{
+    
+    public class UnitTestDemo
+    {
+        public static void Main()
+        {
+            UnitTestDemo demo = new UnitTestDemo();
+            int result=demo.Add(10, 10);
+            Console.WriteLine(result);
+        }
+        public int Add(int a,int b)
+        {
+            return a + b;
+        }
+    }
+}
