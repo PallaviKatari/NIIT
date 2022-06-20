111fe544503ed04e15336d1a17b0b70fe5058d6a
PallaviKatari <pallavikatari@gmail.com>
2022-06-20 10:26:01 +0530

    Commit_Ternary

--- Ternary.cs
+++ Ternary.cs
@@ -11,14 +11,14 @@ namespace NIIT
         public static void Main()
         {
             //shortcut to if-else ? :
-            int num1 = 100;
-            int num2 = 1000;
-            int num3 = num1>num2 ? 1 : 0;
-            Console.WriteLine(Convert.ToBoolean(num3));
-            string num4 = num1 > num2 ? "num1 is greater" : "num2 is greater";
-            Console.WriteLine(num4);
-            var num5 = num1 > num2 ? $"{num1} is greater" : $"{num2} is greater";
-            Console.WriteLine(num5);
+            //int num1 = 100;
+            //int num2 = 1000;
+            //int num3 = num1>num2 ? 1 : 0;
+            //Console.WriteLine(Convert.ToBoolean(num3));
+            //string num4 = num1 > num2 ? "num1 is greater" : "num2 is greater";
+            //Console.WriteLine(num4);
+            //var num5 = num1 > num2 ? $"{num1} is greater" : $"{num2} is greater";
+            //Console.WriteLine(num5);
         }
     }
 }
