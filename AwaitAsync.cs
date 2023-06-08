0c5a7ea0edd8e5413c1763328609cbf584b581e3
PallaviKatari <pallavikatari@gmail.com>
2023-06-08 10:22:59 +0530

    AsyncAwait

---
+++ AwaitAsync.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NIIT
+{
+
+    class AwaitAyncDemo
+    {
+        public static async Task Main()
+        {
+            Sample();
+            await Task.Delay(2000);
+            Console.WriteLine("Finished.");
+
+
+        }
+        static async Task Sample()
+        {
+            Console.WriteLine("Starting...");
+           
+
+        }
+    }
+
+}
